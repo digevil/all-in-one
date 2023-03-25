da5c57b05fecf171a6ac9335209b9980c535d9a8
digevil <digevil@gmail.com>
2023-03-25 16:34:19 +0800

    add spring 3 module and make it build

---
+++ service-boot3/src/main/java/org/digevil/serviceboot3/ServiceBoot3Application.java
@@ -0,0 +1,13 @@
+package org.digevil.serviceboot3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServiceBoot3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServiceBoot3Application.class, args);
+	}
+
+}
